07b3dfb2e7daeea3eed6eb2a11adf16cbff2faa0
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 00:52:24 +0600

    3_Capture Image Using Camera - Android Development with Java - Rakibul Islam - Bohubrihi
    3_Capture Image Using Camera - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/threads/MainActivity.java
+++ app/src/main/java/com/sujon/threads/MainActivity.java
@@ -1,9 +1,15 @@
 package com.sujon.threads;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.VideoView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button cameraButton;
     ImageView myImageView;
 
+    public static final int CAMERA_REQUEST_CODE = 12;
 
 
     @Override
@@ -25,10 +32,35 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (!hasSystemFeature()) {
+                    cameraButton.setEnabled(false);
+                } else {
+                    Intent myIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    if (myIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(myIntent, CAMERA_REQUEST_CODE);
+
+                    }
+
+                }
+
             }
         });
+    }
 
+    public boolean hasSystemFeature() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) bundle.get("data");
+            myImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
