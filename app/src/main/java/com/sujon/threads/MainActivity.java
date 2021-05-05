07965a14cf4765a87798a40fb882028fb378e97a
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-05 22:56:00 +0600

    1_play a video - Android Development with Java - Rakibul Islam - Bohubrihi
    1_play a video - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/threads/MainActivity.java
+++ app/src/main/java/com/sujon/threads/MainActivity.java
@@ -1,13 +1,23 @@
 package com.sujon.threads;
 
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView myVideoView;
+
+    String videoPath = "http://techslides.com/demos/sample-videos/small.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myVideoView = findViewById(R.id.videoView);
+
+        myVideoView.setVideoPath(videoPath);
+        myVideoView.start();
     }
 }
